0c43c895903b0613d88ae3e892853517f9013ea3
Jay Allen <jay@endevver.com>
2009-10-18 06:22:18 -0700

    Eliminated testing code for actual code.  Thrilling.

--- plugins/SubscriberNotice/lib/SubscriberNotice.pm
+++ plugins/SubscriberNotice/lib/SubscriberNotice.pm
@@ -6,24 +6,33 @@ use warnings;
 
 sub update_notifcation {
     my ($cb, $obj, $orig) = @_;
+    my $plugin = MT->component(__PACKAGE__);
+    my $notify_addr
+        = $plugin->get_config_value('notification_email','blog:'.$obj->blog_id);
+    return unless $notify_addr;
+
     my $unsub = $cb->method eq 'MT::Notification::post_remove' ? 1 : 0;
-    my $notify_addr = 'jay@endevver.com';
     my $email       = $obj->email;
+    my $blogname  = MT->model('blog')->load($obj->blog_id)
+                 || 'Blog ID # '.$obj->blog_id;
+    
     my %head        = (
         From    => MT->config->EmailAddressMain,
         To      => $notify_addr,
-        Subject
-            => MT->translate("Betawave investor [_1]: [_2]",
-                        ($unsub ? 'unsubscription' : 'subscription'), $email)
+        Subject => MT->translate("[_1] [_2]: [_3]",
+                        $blogname, 
+                        ($unsub ? 'unsubscription' : 'subscription'), 
+                        $email)
     );
     my $charset = MT->config('MailEncoding')
         || MT->config('PublishCharset');
     $head{'Content-Type'} = qq(text/plain; charset="$charset");
     
     my $body = MT->translate(
-        '[_1] has [_2] the Betawave Investor mailing list.',
+        '[_1] has [_2] the notification list for [_3].',
             $obj->email, 
-            ($unsub ? 'unsubscribed from' : 'subscribed to')
+            ($unsub ? 'unsubscribed from' : 'subscribed to'),
+            $blogname
     );
     # print STDERR Dumper(\%head);
     # print STDERR $body."\n";
